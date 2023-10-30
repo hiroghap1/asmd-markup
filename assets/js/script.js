29f355b440aa390e6040f88991a1ecbfc0078fb2
hiroghap1 <hiro_ghap1@yahoo.co.jp>
2023-10-30 12:49:53 +0900

    test(script.js):GSAP解除

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,29 +7,29 @@ const swiper = new Swiper('#swiper', {
 
 });
 
-gsap.to('.section-company',{backgroundColor: '#fff',
-    scrollTrigger:{
-        trigger:'.section-company',
-        start:'top 40%',
-        end:'bottom 20%',
-        toggleActions:'play none none reverse',
-    }});
-
-gsap.utils.toArray('.fadeIn')
-    .forEach((item) => {
-        gsap.fromTo(item,
-            {
-                opacity: 0,
-                y: 20,
-            },
-            {
-                opacity: 1,
-            y: 0,
-            scrollTrigger: {
-                trigger: item,
-                start: 'top 50%',
-                end: 'bottom 20%',
-                toggleActions: 'play none none reverse',
-            }
-        });
-    });
+// gsap.to('.section-company',{backgroundColor: '#fff',
+//     scrollTrigger:{
+//         trigger:'.section-company',
+//         start:'top 40%',
+//         end:'bottom 20%',
+//         toggleActions:'play none none reverse',
+//     }});
+//
+// gsap.utils.toArray('.fadeIn')
+//     .forEach((item) => {
+//         gsap.fromTo(item,
+//             {
+//                 opacity: 0,
+//                 y: 20,
+//             },
+//             {
+//                 opacity: 1,
+//             y: 0,
+//             scrollTrigger: {
+//                 trigger: item,
+//                 start: 'top 50%',
+//                 end: 'bottom 20%',
+//                 toggleActions: 'play none none reverse',
+//             }
+//         });
+//     });
